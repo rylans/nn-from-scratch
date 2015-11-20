28e96d0675fa2de84b5a9ada8c0ae3799c0525c0
Rylan <rylans@gmail.com>
2015-11-20 11:52:15 -0800

    back prop attempt

--- net.py
+++ net.py
@@ -36,15 +36,62 @@ class Net(object):
         7
         '''
         layer_inputs = inputs
+        all_layer_activations = []
         for layer in self.layers:
             layer_activations = [n.out(layer_inputs) for n in layer]
             layer_inputs = layer_activations
-        return layer_activations
+            all_layer_activations.append(layer_activations)
+        return all_layer_activations
 
-    def __repr__(self):
-        return "Net({0}, input dim={1})".\
+    def train_1(self, inputs, target):
+        predictions = self.feedforward(inputs)
+
+        previous_activations = predictions[:-1]
+        previous_activations.reverse()
+
+        layers_reversed = self.layers[::-1]
+        #print "zip shape {0}".format([(len(k), len(j)) for k,j in zip(layers_reversed, previous_activations)])
+
+        #Product of the previous error and previous output derivative
+        error_der = [1.0 for k in previous_activations[0]]
+        for layer, prev_activation in zip(layers_reversed, previous_activations):
+            #activs = [p*e for p, e in zip(prev_activation, error_der)]
+            target = target*error_der[0]
+            error_der = []
+            for neuron in layer:
+                #print "prev activation len {0} for neuron: {1}".format(len(prev_activation), neuron)
+                dE, dy = neuron.learn_1(prev_activation, target)
+                error_der.append(dE*dy)
+
+    def __repr__(self): return "Net({0}, input dim={1})".\
                 format(str([len(l) for l in self.layers]), self.input_size)
+
+def training_trial_1(steps):
+    import random
+    input_dim = 2
+    net = Net([4, 2, 1], input_dim)
+    print net.feedforward([0.1,0.8])[-1]
+    for i in range(steps):
+        ins = [random.random() for k in range(input_dim)]
+        target = sum(ins)
+        net.train_1(ins, target)
+    print net.feedforward([0.1,0.8])[-1]
+
+def training_trial_2(steps):
+    import random
+    input_dim = 2
+    net = Net([4, 2, 1], input_dim)
+    print net.feedforward([0.1,0.8])[-1]
+    for i in range(steps):
+        ins = [random.random() for k in range(input_dim)]
+        target = min(ins)
+        net.train_1(ins, target)
+    print net.feedforward([0.1,0.8])[-1]
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+    training_trial_1(200)
+    print
+    training_trial_2(700)
